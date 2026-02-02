09679bad65fcdd9d79364b37ed7255c7902efc89
priyanshu4492 <priyanshudangi449244@gmail.com>
2026-02-02 22:56:36 +0530

    added transparent image

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -7,17 +7,22 @@ class StartScreen extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Image.asset('assets/images/quiz-logo.png', width: 300),
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(153, 236, 231, 231),
+          ),
           const SizedBox(height: 50),
           const Text(
             'Learn Fluttr the fun way!',
             style: TextStyle(color: Colors.white, fontSize: 24),
           ),
 
-          OutlinedButton(
+          OutlinedButton.icon(
             onPressed: () {},
             style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
-            child: Text('start quiz'),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('start quiz'),
           ),
         ],
       ),
